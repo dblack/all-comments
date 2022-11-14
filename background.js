57d1102af6f35be964feb066769be0e2e17fc5a4
David A. Black <david@davidablack.net>
2022-11-14 05:02:17 -0500

    Remove console logging and extraneous async function

--- background.js
+++ background.js
@@ -1,22 +1,16 @@
 /*jshint esnext: true */
 
 async function showAllComments() {
-  const clickMe = async function(element) {
-    await(element.click())
-  }
   var s = document.querySelectorAll('span');
   s.filter = Array.prototype.filter;
   var r = s.filter((x) => { return x.innerText.trim() == 'Most relevant' })
-  console.log(r[0])
-  await clickMe(r[0])
+  await r[0].click()//clickMe(r[0])
   document.querySelectorAll('span').forEach(
     (e) => { 
       if (e.innerText.trim() == 'All comments') {
-        console.log('here')
         e.click()
       } 
     })
-  console.log("ran");
 }
 
 chrome.action.onClicked.addListener((tab) => {
